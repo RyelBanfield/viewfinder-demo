962dde8f33d80483a40336c0c000b7808a93f987
Ryel Banfield <ryelbanfield@gmail.com>
2022-09-28 23:48:47 -0400

    Revert "Use getServerSideProps for SSR the images"
    This reverts commit e366ebb31e0cbed7c2d4a473ec4d5a4834d6d9bf.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,13 @@
-import { collection, getDocs } from 'firebase/firestore';
+import { NextPage } from 'next';
 
+import Gallery from '../components/Gallery';
 import Hero from '../components/Hero';
-import UserImage from '../components/UserImage';
-import { db } from '../firebase';
 
-const Home = ({ images }: { images: [{ uid: string; url: string }] }) => (
+const Home: NextPage = () => (
   <main className="flex flex-grow flex-col">
     <Hero />
-    <div className="mb-6 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-      {images.map((image) => (
-        <UserImage key={image.url} image={image} />
-      ))}
-    </div>
+    <Gallery />
   </main>
 );
-
-export const getServerSideProps = async () => {
-  const images: { uid: string; url: string }[] = [];
-  const querySnapshot = await getDocs(collection(db, 'images'));
-  querySnapshot.forEach((doc) => {
-    images.push({
-      uid: doc.data().uid,
-      url: doc
-        .data()
-        .url.replace(
-          'https://firebasestorage.googleapis.com',
-          'https://ik.imagekit.io/zuge4mgxf/',
-        ),
-    });
-  });
-
-  return {
-    props: { images },
-  };
-};
 
 export default Home;
