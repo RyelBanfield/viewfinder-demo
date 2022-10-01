afd7ce739903f56850108bb728584d38716d3813
Ryel Banfield <ryelbanfield@gmail.com>
2022-10-01 13:17:44 -0400

    Remove createImage cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,19 +10,6 @@ export const deleteUserDoc = functions.auth.user().onDelete((user) => {
   db.collection('users').doc(user.uid).delete();
 });
 
-export const createImageDoc = functions.storage
-  .object()
-  .onFinalize((object) => {
-    const { name, bucket } = object;
-    const url = `https://firebasestorage.googleapis.com/v0/b/${bucket}/o/${name}?alt=media`;
-    const uid = name!.split('_')[0];
-
-    db.collection('images').doc(name).set({
-      uid,
-      url,
-    });
-  });
-
 export const deleteImageDoc = functions.storage.object().onDelete((object) => {
   const { name } = object;
   db.collection('images').doc(name).delete();
