7256cc6f821c29281d673ccad7187b2b5f2120ba
Ryel Banfield <ryelbanfield@gmail.com>
2022-12-25 14:25:16 -0400

    Add id to Image type

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
-import { collection, getDocs } from 'firebase/firestore/lite';
-import { NextPage } from 'next';
+import { collection, getDocs } from "firebase/firestore/lite";
+import { NextPage } from "next";
 
-import Gallery from '../components/Gallery';
-import { db } from '../firebase';
+import Gallery from "../components/Gallery";
+import { db } from "../firebase";
 
 export type Image = {
+  id: string;
+  uid: string;
   username: string;
   firstName: string;
   lastName: string;
@@ -28,18 +30,20 @@ const Home: NextPage<{ randomHeroImage: string; images: Image[] }> = ({
 export const getServerSideProps = async () => {
   const imageData: Image[] = [];
 
-  const querySnapshot = await getDocs(collection(db, 'images'));
+  const querySnapshot = await getDocs(collection(db, "images"));
 
   querySnapshot.forEach((doc) => {
     imageData.push({
+      id: doc.id,
+      uid: doc.data().uid,
       username: doc.data().username,
       firstName: doc.data().firstName,
       lastName: doc.data().lastName,
       url: doc
         .data()
         .url.replace(
-          'https://firebasestorage.googleapis.com',
-          'https://ik.imagekit.io/zuge4mgxf',
+          "https://firebasestorage.googleapis.com",
+          "https://ik.imagekit.io/zuge4mgxf"
         ),
     });
   });
