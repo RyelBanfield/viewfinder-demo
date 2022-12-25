75699cb6ffb88c05129422ce60d6a6c4bf0626fb
Ryel Banfield <ryelbanfield@gmail.com>
2022-12-25 13:52:29 -0400

    Update node v

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,16 +1,15 @@
-import * as functions from 'firebase-functions';
-
-const admin = require('firebase-admin');
+import * as admin from "firebase-admin";
+import * as functions from "firebase-functions";
 
 admin.initializeApp();
 
 const db = admin.firestore();
 
 export const deleteUserDoc = functions.auth.user().onDelete((user) => {
-  db.collection('users').doc(user.uid).delete();
+  db.collection("users").doc(user.uid).delete();
 });
 
 export const deleteImageDoc = functions.storage.object().onDelete((object) => {
-  const { name } = object;
-  db.collection('images').doc(name).delete();
+  const { name } = object as { name: string };
+  db.collection("images").doc(name).delete();
 });
