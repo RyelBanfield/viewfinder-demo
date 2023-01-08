e27ce913c081ac59a1cceafdb89965f0247fff20
Ryel Banfield <ryelbanfield@gmail.com>
2023-01-08 14:47:26 -0400

    Add router to dep array

--- src/ProtectedRoute.tsx
+++ src/ProtectedRoute.tsx
@@ -9,7 +9,7 @@ const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
 
   useEffect(() => {
     if (!user) router.push("/login");
-  }, [user]);
+  }, [router, user]);
 
   return <>{user ? children : null}</>;
 };
